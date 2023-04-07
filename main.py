1ed825a380e1fe42e4b75ba7147339e88a513ab9
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-04-07 05:03:31 +0200

    Snake setup at the starting position.

--- main.py
+++ main.py
@@ -3,4 +3,33 @@ import random
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+starting_positions = [(0.0, 0.0), (-20.0, 0.0), (-40.0, 0.0)]
+
+for position in starting_positions:
+    segment = Turtle("square")
+    segment.color("white")
+    segment.goto(position)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 screen.exitonclick()
