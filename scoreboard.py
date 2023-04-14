a861408d78ac0dd7a88917fbb4840671743bd24f
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-04-14 12:19:13 +0200

    Adding high score function to the game.

--- scoreboard.py
+++ scoreboard.py
@@ -13,14 +13,17 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.color("white")
         self.goto(0, 270)
+        self.high_score = 0
         # self.scoring()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0.0, 0.0)
-        self.write("Game Over!", move=False, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+
 
     def scoring(self):
         self.score += 1
