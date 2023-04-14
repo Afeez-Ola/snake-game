1b9851d2aad15920b2da65c21431c341165ea1b5
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-04-14 14:28:20 +0200

    Storing the high_score in a file.

--- scoreboard.py
+++ scoreboard.py
@@ -4,6 +4,12 @@ ALIGNMENT = "center"
 FONT = ("Arial", 18, "normal")
 
 
+def read_high_score():
+    with open("file.txt", mode="r") as file:
+        content = file.read()
+        return content
+
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
@@ -12,33 +18,25 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.color("white")
         self.goto(0, 270)
-        self.high_score = 0
-        self.read_high_score()
-        # self.scoring()
+        self.high_score = int(read_high_score())
 
     def update_scoreboard(self):
-        self.write_high_score()
         self.clear()
         self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
     def reset(self):
-        if self.score > self.high_score:
+        if self.score > int(read_high_score()):
             self.high_score = self.score
+            self.update_score()
         self.score = 0
         self.update_scoreboard()
-        self.read_high_score()
-
-    def write_high_score(self):
-        with open("file.txt", mode="w") as file:
-            file.write(f"{self.high_score}")
-
-    def read_high_score(self):
-        with open("file.txt", mode="r") as file:
-            file.read()
 
     def scoring(self):
         self.score += 1
         self.update_scoreboard()
 
-
-# print(Scoreboard().score)
+    def update_score(self):
+        if self.high_score > int(read_high_score()):
+            with open("file.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+            read_high_score()
